91b57017cf145d7207cc1a51087b1d696610f441
MyFlyingEye <MyFlyingEye@users.noreply.github.com>
2016-06-27 19:50:48 +0300

    sysv

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:
-	g++ /home/box/temp/pipes_bf.cpp -std=c++11 -o /home/box/shell
-	cp /home/box/temp/Makefile /home/box/Makefile
---
+++ sysv_msgq.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+#define MSG_PERM 0666
+
+int main() {
+  key_t key = ftok("/tmp/msg.temp", 1);
+
+  int msgq = msgget(key, MSG_PERM | IPC_CREAT);
+
+  struct my_msg {
+    long mtype;
+    char mtext[80];
+  } data;
+  bzero(&data, sizeof(data));
+
+  ssize_t msglen = msgrcv(msgq, &data, 80, 0, 0);
+  if (msglen > 0) {
+    perror("msgrcv");
+    exit(EXIT_FAILURE);
+  }
+
+  int f = open("/home/box/message.txt", O_WRONLY | O_TRUNC);
+
+  write(f, data.mtext, msglen);
+
+  close(f);
+
+  exit(EXIT_SUCCESS);
+}
