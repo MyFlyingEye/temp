00e16b760ca99e1fb492a9ab5dadaa8bc3fe0d82
MyFlyingEye <MyFlyingEye@users.noreply.github.com>
2016-06-28 17:41:37 +0300

    sysv sem

--- sysv_sem.c
+++ sysv_sem.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <unistd.h>
 
 #define SEMNUM 16
+#define PERM 0666
 
 int main() {
   key_t key = ftok("/tmp/sem.temp", 1);
 
-  int semid = semget(key, SEMNUM, IPC_CREAT);
+  int semid = semget(key, SEMNUM, PERM | IPC_CREAT);
 
   union semun {
     int val;
@@ -16,10 +18,15 @@ int main() {
     struct seminfo* _buf;
   } arg;
 
-  for (int num = 0; num < SEMNUM; ++num) {
+  int num;
+  for (num = 0; num < SEMNUM; ++num) {
     arg.val = num;
-    semctl(semid, num, SETVAL, arg.val);
+    semctl(semid, num, SETVAL, arg);
   }
+
+  sleep(10);
+
+  semctl(semid, 0, IPC_RMID);
 
   exit(EXIT_SUCCESS);
 }
