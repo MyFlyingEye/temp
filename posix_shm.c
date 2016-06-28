3180f49c16cb72b3d28e0fa81dd05ebc4c9d13a4
MyFlyingEye <MyFlyingEye@users.noreply.github.com>
2016-06-28 19:41:01 +0300

    posix shm

--- posix_shm.c
+++ posix_shm.c
@@ -4,14 +4,23 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #define SHMNAME "/test.shm"
 #define SHMSZ 1048576
 
 int main() {
-  int id = shm_open(SHMNAME, O_CREAT, ALLPERMS);
+  int id = shm_open(SHMNAME, O_CREAT | O_RDWR, ALLPERMS);
+  if (id < 0) {
+    perror("shm_open");
+    exit(EXIT_FAILURE);
+  }
   ftruncate(id, SHMSZ);
   void* addr = mmap(NULL, SHMSZ, PROT_WRITE, MAP_SHARED, id, 0);
+  if (addr == MAP_FAILED) {
+    perror("mmap");
+    exit(EXIT_FAILURE);
+  }
 
   memset(addr, 13, SHMSZ);
 
